e0a1532bbb7cf7c7a87189a3d84a30ef291a7c08
Chad Wilson <cwilson66@mail.csuchico.edu>
2019-02-21 11:48:41 -0600

    Creates prototype of getting login info and reimbursment requests from the user

---
+++ project1/src/main/java/com/revature/App.java
@@ -0,0 +1,37 @@
+package com.revature;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try(InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+        ) {
+            System.out.println("Please enter your login information! Add a space between username and password");
+            String loginInfo = br.readLine();
+            Scanner scanner = new Scanner(loginInfo);
+            String username = scanner.next();
+            String password = scanner.next();
+            System.out.println("Credentials you entered: Username: " + username + " Password: " + password);
+            System.out.println("Hello " + username + "! Please enter your reimbursment request. Add a space between your id, your manager's id, and the amount of money you want reimbursed.");
+            String reimbursmentRequest = br.readLine();
+            scanner = new Scanner(reimbursmentRequest);
+            String employeeId = scanner.next();
+            String managerId = scanner.next();
+            String money = scanner.next();
+            System.out.println("Credentials you entered: EmployeeId: " + employeeId + " ManagerId: " + managerId + " Money: " + money);
+            System.out.println("Thank you for your reimbursment request!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
